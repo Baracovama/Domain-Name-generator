01ed93bd8c82387f6d144635b91d0b2897f16bbe
Baracovama <juanvguevaram@gmail.com>
2022-06-19 18:49:31 +0000

    Proyecto Finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var dom = [".com", ".net", ".es"];
+
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+      for (var y = 0; y < noun.length; y++) {
+        for (var x = 0; x < dom.length; x++) {
+          console.log(pronoun[i] + adj[j] + noun[y] + dom[x]);
+        }
+      }
+    }
+  }
 };
